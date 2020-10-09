5eb3bfda19b89c8d5b0813c4d74ea6fdd92e4df9
skypekitten9 <victor.enhorning@gmail.com>
2020-10-09 16:01:11 +0200

    Moved packages and created a script to generate tiles.

---
+++ Assets/Scripts/spawnGrid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnGrid : MonoBehaviour
+{
+    public GameObject tile;
+    public int xSize, zSize;
+    public float spacing;
+    private void Start()
+    {
+        StartCoroutine(SpawnGrid());
+    }
+
+    IEnumerator SpawnGrid()
+    {
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int z = 0; z < zSize; z++)
+            {
+                Vector3 pos = new Vector3(x * spacing, 0, z * spacing);
+                SpawnObj(pos, Quaternion.identity);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        
+    }
+
+    void SpawnObj(Vector3 spawnPosition, Quaternion spawnRotation)
+    {
+        GameObject obj = Instantiate(tile, spawnPosition, spawnRotation);
+        obj.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+}
