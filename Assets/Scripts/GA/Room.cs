39764f0a334cb1ea43f1a60856b22ddb41709893
skypekitten9 <victor.enhorning@gmail.com>
2020-11-09 11:18:55 +0100

    Reworked room script, it now uses 2-dim array instead.

--- Assets/Scripts/GA/Room.cs
+++ Assets/Scripts/GA/Room.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 
 public class Room
 {
-    public string[] furniture { get; private set; }
+    public string[,] furniture { get; private set; }
     public Vector2[] furnitureSize { get; private set; }
     int width, height;
     GameObject gridSpawner;
     public Room(int width, int height, GameObject gridSpawner)
     {
-        furniture = new string[width * height];
+        furniture = new string[height , width];
         this.width = width;
         this.height = height;
         this.gridSpawner = gridSpawner;
@@ -25,32 +25,35 @@ public class Room
 
     public void GenerateRoom()
     {
-        for (int i = 0; i < furniture.Length; i++)
+        for (int i = 0; i < height; i++)
         {
-            switch (UnityEngine.Random.Range(0,6))
+            for (int j = 0; j < width; j++)
             {
-                case 0:
-                    furniture[i] = "N";
-                    break;
-                case 1:
-                    furniture[i] = "E";
-                    break;
-                case 2:
-                    furniture[i] = "S";
-                    break;
-                case 3:
-                    furniture[i] = "W";
-                    break;
-                case 4:
-                    furniture[i] = "0";
-                    break;
-                case 5:
-                    furniture[i] = "X";
-                    break;
-                default:
-                    break;
+                switch (UnityEngine.Random.Range(0, 6))
+                {
+                    case 0:
+                        furniture[i, j] = "N";
+                        break;
+                    case 1:
+                        furniture[i, j] = "E";
+                        break;
+                    case 2:
+                        furniture[i, j] = "S";
+                        break;
+                    case 3:
+                        furniture[i, j] = "W";
+                        break;
+                    case 4:
+                        furniture[i, j] = "0";
+                        break;
+                    case 5:
+                        furniture[i, j] = "X";
+                        break;
+                    default:
+                        break;
+                }
+                furniture[i, j] += UnityEngine.Random.Range(0, gridSpawner.GetComponent<spawnGrid>().furnitureArray.Length);
             }
-            furniture[i] += UnityEngine.Random.Range(0, gridSpawner.GetComponent<spawnGrid>().furnitureArray.Length);
         }
     }
 
@@ -58,22 +61,22 @@ public class Room
     {
         for (int i = 0; i < furniture.Length; i++)
         {
-           
+            
         }
     }
 
-    public Room CrossOver(Room parent)
+    public Room CrossOver(Room parent) //EJ IMPLEMENTERAD KORREKT ÄN
     {
         Room child = new Room(width, height, gridSpawner);
         for (int i = 0; i < furniture.Length; i++)
         {
             if (i < furniture.Length/2)
             {
-                child.furniture[i] = furniture[i];
+                child.furniture[i, i] = furniture[i, i];
             }
             else
             {
-                child.furniture[i] = parent.furniture[i];
+                child.furniture[i, i] = parent.furniture[i, i];
             }
         }
         return child;
@@ -82,14 +85,12 @@ public class Room
     public string GetRoomString()
     {
         string result = gridSpawner.name + " " + height + " " + width + ";\n";
-        int index = 0;
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
                 if (j != 0) result += " ";
-                result += furniture[index];
-                index++;
+                result += furniture[i, j];
             }
             result += ";\n";
         }
