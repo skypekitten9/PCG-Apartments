27130fcc102d47fd347e387d6db01f3d326f636b
skypekitten9 <victor.enhorning@gmail.com>
2021-01-25 22:24:08 +0100

    Added UI and methods to update them.

--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UserInterface : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject generation, population, fitness;
+    TextMeshProUGUI generationText, populationText, fitnessText;
     void Start()
     {
-        
+        generationText = generation.GetComponent<TextMeshProUGUI>();
+        populationText = population.GetComponent<TextMeshProUGUI>();
+        fitnessText = fitness.GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetGeneration(int generation)
     {
-        
+        generationText.text = generation.ToString();
+    }
+
+    public void SetPopulation(int population)
+    {
+        populationText.text = population.ToString();
+    }
+
+    public void SetFitness(float fitness)
+    {
+        populationText.text = fitness.ToString();
     }
 }
