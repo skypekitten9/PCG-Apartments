9ef4959306e101e9b9cfdf747edbbbbce101e9c1
PatrickCx <patrickglooglass@gmail.com>
2021-01-26 21:06:25 +0100

    gave furniture a set

--- Assets/Scripts/Furniture.cs
+++ Assets/Scripts/Furniture.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class Furniture : MonoBehaviour
 {
     public int width, height;
+    public string set;
 }
--- Assets/Scripts/GA/Room.cs
+++ Assets/Scripts/GA/Room.cs
@@ -58,6 +58,9 @@ public class Room
             {
                 chairCount++;
             }
+
+           
+
         }
 
     }
