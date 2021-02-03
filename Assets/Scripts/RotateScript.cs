d56789a3d14a393d7c44d5b069262128f7f0e3d3
skypekitten9 <victor.enhorning@gmail.com>
2021-02-03 10:52:45 +0100

    Added camera control + prefabs + readme.

---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    public float speed, autoSpeed;
+    public bool autoRotate;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+        else if (Input.GetKey(KeyCode.D))
+            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+
+        else if(Input.GetKey(KeyCode.R))
+        {
+            autoRotate = !autoRotate;
+        }
+        else
+        {
+            if(autoRotate) transform.Rotate(-Vector3.up * autoSpeed * Time.deltaTime);
+        }
+    }
+}
