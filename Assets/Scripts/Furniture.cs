c5710e526d8b841ce0c778727c7f6f5ae75d8589
skypekitten9 <victor.enhorning@gmail.com>
2021-02-18 17:59:20 +0100

    Added furnitureType.

--- Assets/Scripts/Furniture.cs
+++ Assets/Scripts/Furniture.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum FamilyType { Dining, Parlour, None }
+public enum FurnitureType { Chair, Table, TV, Lamp, Sofa, None }
 public class Furniture : MonoBehaviour
 {
     public int width, height;
     public FamilyType familyType;
+    public FurnitureType furnitureType;
 }
