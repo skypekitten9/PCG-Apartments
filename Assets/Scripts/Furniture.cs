783009828e28bb687ad9aef19ac96c1f3b08faba
skypekitten9 <victor.enhorning@gmail.com>
2021-02-20 10:39:41 +0100

    Added more attributes to furniture.

--- Assets/Scripts/Furniture.cs
+++ Assets/Scripts/Furniture.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum FamilyType { Dining, Parlour, None }
+public enum FamilyType { Surrounding, Directional, None }
 public enum FurnitureType { Chair, Table, TV, Lamp, Sofa, None }
 public class Furniture : MonoBehaviour
 {
     public int width, height;
     public FamilyType familyType;
     public FurnitureType furnitureType;
+    public FurnitureType likes;
+    public int prefferedDistance;
 }
